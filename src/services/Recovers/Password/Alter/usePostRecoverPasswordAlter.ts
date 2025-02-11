09d6fa24adfa2939a406aba4da63f5475eda2a20
igfernandes <fernandes7.1@outlook.com>
2025-02-11 13:51:47 -0300

    feat: Created page successfull

--- src/services/Recovers/Password/Alter/usePostRecoverPasswordAlter.ts
+++ src/services/Recovers/Password/Alter/usePostRecoverPasswordAlter.ts
@@ -3,11 +3,17 @@ import { useSnackbar } from "@hooks/useSnackbar";
 import { useAxios } from "@hooks/useAxios";
 import { PostRecoverPasswordAlterPayload } from "./type";
 import { usePostRecoverPasswordAlterService } from ".";
+import { useRouter } from "next/router";
+import { userRoutes } from "@configs/routes/Web/navigation";
+import { useTranslation } from "next-i18next";
 
 export default function usePostRecoverPasswordAlter() {
+  const { t } = useTranslation("common");
   const { handleAxiosError } = useAxios();
   const { dispatchSnackbar } = useSnackbar();
   const { postRecoverPasswordAlter } = usePostRecoverPasswordAlterService();
+  const router = useRouter();
+  const { successful } = userRoutes;
 
   const handleMutate = async (payload: PostRecoverPasswordAlterPayload) => {
     const { data } = await postRecoverPasswordAlter(payload);
@@ -18,10 +24,12 @@ export default function usePostRecoverPasswordAlter() {
   return useMutation(handleMutate, {
     onSuccess: () => {
       dispatchSnackbar({
-        message: "Você será redirecionado",
-        title: "Conectado com Sucesso",
+        message:  t("texts.will_redirect"),
+        title: t("words.success"),
         type: "success",
       });
+
+      router.push(`${successful}?=type=created_password`);
     },
     onError: (err) => {
       handleAxiosError(err);
