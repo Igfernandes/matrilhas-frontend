d513e83bb3c65da193c5b0cc30fef85d69bb7cba
igfernandes <fernandes7.1@outlook.com>
2025-02-13 18:32:19 -0300

    chore: adding an value default to language how pt-br

--- src/configs/envs/index.ts
+++ src/configs/envs/index.ts
@@ -1,6 +1,6 @@
 import { EnvironmentProprieties, EnvironmentTypes } from "./type";
 
-export const LANGUAGE_I18N = process.env.NEXT_PUBLIC_LANGUAGE;
+export const LANGUAGE_I18N = process.env.NEXT_PUBLIC_LANGUAGE ?? "pt-br";
 export const env: EnvironmentProprieties = {
   PROD: {
     API_URL: process.env.NEXT_PUBLIC_PROD_URL,
