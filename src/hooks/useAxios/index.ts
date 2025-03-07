a968aad62f380a9bde3783767ae8e15487d0f194
igfernandes <fernandes7.1@outlook.com>
2025-03-07 11:22:45 -0300

    chore: Adding service version to process with hook and without hook part2

--- src/hooks/useAxios/index.ts
+++ src/hooks/useAxios/index.ts
@@ -3,18 +3,26 @@ import { useSnackbar } from "../useSnackbar";
 import { CustomAxiosError, ShapeError } from "./type";
 import { env } from "@configs/envs";
 import i18n from "@configs/i18n";
+import { STATUS_SERVICE } from "@constants/services";
+import { isValidJSON } from "@helpers/json";
+
+const axiosConfig = {
+  baseURL: env.API_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+};
+
+export const axios = new Axios(axiosConfig);
 
 export function useAxios() {
   const { dispatchSnackbar } = useSnackbar();
-
   const axios = new Axios({
-    baseURL: env.API_URL,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    },
+    ...axiosConfig,
+    validateStatus: (status: number) =>
+      status >= STATUS_SERVICE.OK && status < STATUS_SERVICE.REDIRECT,
   });
-
   /**
    * @function handleAxiosError
    * - Irá analisar o erro e tratar baseado no modelo de resposta do axios.
@@ -29,11 +37,14 @@ export function useAxios() {
       message: i18n("errors.service.default.message") as string,
     };
     const typedError = error as CustomAxiosError;
-    const responseData = typedError.response?.data;
+    const jsonResponseData = typedError.response?.data ?? "";
+    const responseData = isValidJSON(jsonResponseData)
+      ? JSON.parse(jsonResponseData)
+      : null;
 
-    const responseError = !!responseData && responseData.error;
+    const responseError = !!responseData && responseData.errors;
     if (responseError) {
-      shapeError["message"] = responseError;
+      shapeError["message"] = responseError as string;
     }
 
     dispatchSnackbar({ ...shapeError, type: "error" });
