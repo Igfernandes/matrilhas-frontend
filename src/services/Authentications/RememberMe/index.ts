b98faf0d76605f7f8a278ecde9ee2081b1dfe189
igfernandes <fernandes7.1@outlook.com>
2025-03-07 11:22:23 -0300

    chore: Adding service version to process with hook and without hook

--- src/services/Authentications/RememberMe/index.ts
+++ src/services/Authentications/RememberMe/index.ts
@@ -1,10 +1,25 @@
-import { useAxios } from "@hooks/useAxios";
+import { axios } from "@hooks/useAxios";
 import { PostRememberMePayload } from "./type";
 import { authenticationRoutes } from "@configs/routes/Api/authentications";
 import { getPayloadJSON } from "@helpers/payload";
+import { STATUS_SERVICE } from "@constants/services";
+
+export async function postRememberMe({
+  referenceToken,
+}: PostRememberMePayload): Promise<boolean> {
+  const { rememberMe } = authenticationRoutes;
+
+  const { status } = await axios.post(
+    rememberMe,
+    getPayloadJSON({
+      "reference-token": referenceToken,
+    })
+  );
+
+  return status == STATUS_SERVICE.OK;
+}
 
 export function usePostRememberMeService() {
-  const { axios } = useAxios();
   const { rememberMe } = authenticationRoutes;
 
   async function postRememberMe({ referenceToken }: PostRememberMePayload) {
