dae6b10bcb922076336e417d3f01fbcfa499071d
igfernandes <fernandes7.1@outlook.com>
2025-06-09 18:52:00 -0300

    feat: atualizações

--- src/hooks/useAxios/interceptores/hasErrorAuthentication.ts
+++ src/hooks/useAxios/interceptores/hasErrorAuthentication.ts
@@ -17,6 +17,7 @@ export function hasErrorAuthentication(error: AxiosError) {
   const url = error?.config?.url;
 
   if (url?.includes(privateRoutes.usersManager) && isStatusValid) {
+    console.log(url, privateRoutes.usersManager, url?.includes(privateRoutes.usersManager))
     handleLogout();
     return (window.location.href = publicRoutes.login);
   }
