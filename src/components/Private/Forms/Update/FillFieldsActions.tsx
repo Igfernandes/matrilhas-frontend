6b64e5dc38e1dcdfef43ee8cc48bc0ea769c1436
igfernandes <fernandes7.1@outlook.com>
2025-06-24 16:47:57 -0300

    fix: problem with update informations user

--- src/components/Private/Forms/Update/FillFieldsActions.tsx
+++ src/components/Private/Forms/Update/FillFieldsActions.tsx
@@ -63,7 +63,7 @@ export function FillFieldsActions({
     });
 
     setActions(data);
-  }, [serviceId, formId]);
+  }, [serviceId, formId, refPackage, forms]);
 
   return (
     <div className="flex">
