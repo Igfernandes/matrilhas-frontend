3cdd37fb465bee69849d012f9e32bb45d5c43a11
igfernandes <fernandes7.1@outlook.com>
2025-12-04 20:58:48 -0300

    chore: update rules in translater screen

--- src/components/Public/AlterPassword/index.tsx
+++ src/components/Public/AlterPassword/index.tsx
@@ -1,17 +1,13 @@
 import { LockCog } from "@assets/Icons/colorful/LockCog";
 import i18n from "@configs/i18n";
 import { AlterPasswordForm } from "./Form";
-import { useEffect, useState } from "react";
+import { useRef } from "react";
 
 export function AlterPasswordContent() {
-  const [texts, setTexts] = useState<Record<string, string>>({});
-
-  useEffect(() => {
-    setTexts({
-      title: "Screens.alter-password.title",
-      text: "Screens.alter-password.text",
-    });
-  }, []);
+  const texts = useRef<Record<string, string>>({
+    title: "Screens.alter_password.title",
+    text: "Screens.alter_password.text",
+  });
 
   return (
     <>
@@ -20,11 +16,11 @@ export function AlterPasswordContent() {
       </div>
       <div className="mb-1">
         <h2 className="text-2xl">
-          <strong>{i18n(texts?.title)}</strong>
+          <strong>{i18n(texts.current.title)}</strong>
         </h2>
       </div>
       <div className="mb-6">
-        <p className="text-sm">{i18n(texts?.text)}</p>
+        <p className="text-sm">{i18n(texts.current.text)}</p>
       </div>
       <AlterPasswordForm />
     </>
---
+++ src/configs/i18n/locales/pt-br/screens/public/alterPassword.ts
@@ -0,0 +1,5 @@
+export const alterPasswordTranslations = {
+  title: "Nova senha",
+  text: "Crie uma nova senha para acessar a plataforma.",
+  submit: "Criar nova senha",
+};
