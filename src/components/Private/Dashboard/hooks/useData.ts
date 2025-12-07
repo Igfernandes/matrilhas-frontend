ce9dda9ec8536306c0815e9f8065b2cf1ade1d98
igfernandes <fernandes7.1@outlook.com>
2025-12-07 20:08:57 -0300

    chore: new rules and organization in dashboard

---
+++ src/components/Private/Dashboard/hooks/useData.ts
@@ -0,0 +1,26 @@
+import useGetCharges from "@services/Charges/Get/useGetCharges";
+import useGetClients from "@services/Clients/Get/useGet";
+import useGetForms from "@services/CustomForms/Get/useGetForms";
+import useGetUsers from "@services/Users/Get/useGetUsers";
+export function useOverviewData() {
+  const { count: clientsCount, isPending: isLoadingClients } = useGetClients({
+    limit: 0,
+  });
+  const { count: formsCount, isPending: isLoadingForms } = useGetForms({
+    limit: 0,
+  });
+  const { count: chargesCount, isPending: isLoadingCharges } = useGetCharges({
+    limit: 0,
+  });
+  const { count: usersCount, isPending: isLoadingUsers } = useGetUsers({
+    limit: 0,
+  });
+
+  return {
+    clientsCount,
+    formsCount,
+    chargesCount,
+    usersCount,
+    isLoading: isLoadingClients || isLoadingForms || isLoadingCharges || isLoadingUsers,
+  };
+}
