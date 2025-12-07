69d690fdf6c7d1594248c17349e76e3c65b7c93b
igfernandes <fernandes7.1@outlook.com>
2025-12-07 20:06:43 -0300

    chore: update styled and rules in header

--- src/components/Private/Dashboard/Header.tsx
+++ src/components/Private/Dashboard/Header.tsx
@@ -1,52 +1,45 @@
-import { Box } from "@assets/Icons/black/Box";
 import { Form } from "@assets/Icons/black/Form";
 import { UserGroup } from "@assets/Icons/black/UserGroup";
 import { UsersManager } from "@assets/Icons/black/UsersManager";
 import { Wallet } from "@assets/Icons/black/Wallet";
 import { CardAmountBoard } from "@components/shared/layouts/CardAmountBoard";
 import i18n from "@configs/i18n";
-import { ManagerEntitiesProps } from "./type";
+import { ManagerProps } from "./type";
 
 export function DashboardHeader({
   clients,
-  services,
   users,
   forms,
   charges,
-}: ManagerEntitiesProps) {
+  isLoading
+}: ManagerProps) {
   return (
     <CardAmountBoard
-      viewLimit={5}
-      isLoading={!users || !clients || !services || !forms || !charges}
+      viewLimit={4}
+      isLoading={isLoading}
       items={[
         {
           icon: <UserGroup fill="white" />,
           title: i18n("Words.clients"),
-          value: String(clients?.length ?? 0).padStart(6, "0"),
+          value: String(clients).padStart(6, "0"),
         },
         {
           icon: <UsersManager fill="white" />,
           background: "bg-red",
           title: i18n("Words.users"),
-          value: String(users?.length ?? 0).padStart(6, "0"),
-        },
-        {
-          icon: <Box fill="white" />,
-          background: "bg-green",
-          title: i18n("Words.services"),
-          value: String(services?.length ?? 0).padStart(6, "0"),
+          value: String(users).padStart(6, "0"),
         },
         {
           icon: <Form fill="white" />,
           background: "bg-purple",
           title: i18n("Words.forms"),
-          value: String(forms?.length ?? 0).padStart(6, "0"),
+          value: String(forms).padStart(6, "0"),
         },
         {
           icon: <Wallet fill="white" />,
           background: "bg-orange",
           title: i18n("Words.charges"),
-          value: String(charges?.length ?? 0).padStart(6, "0"),
+          value: String(charges).padStart(6, "0"),
         },
       ]}
     />
