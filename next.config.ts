ecf4198d0b21ef5a5ebc5a2167c69293887b44b7
igfernandes <fernandes7.1@outlook.com>
2025-12-31 11:35:14 -0300

    fix: problems with env

--- next.config.ts
+++ next.config.ts
@@ -7,16 +7,19 @@ const nextConfig: NextConfig = {
         hostname: "localhost/**",
       },
       {
-        hostname: "192.168.1.5/**"
+        hostname: "192.168.1.5/**",
       },
       {
-        hostname: "192.168.1.2/**"
-      }, 
-      {
-        hostname: "192.168.1.3/**"
+        hostname: "192.168.1.2/**",
       },
       {
-        hostname: "192.168.1.4/**"
+        hostname: "192.168.1.3/**",
+      },
+      {
+        hostname: "192.168.1.4/**",
+      },
+      {
+        hostname: "api.matrilhas.com.br/**",
       },
     ],
   },
