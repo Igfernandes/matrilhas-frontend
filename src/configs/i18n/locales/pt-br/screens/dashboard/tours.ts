fe5d7f4433aa8be4a62b878fa49be4cae801699b
igfernandes <fernandes7.1@outlook.com>
2025-12-31 12:12:44 -0300

    fix: update

--- src/configs/i18n/locales/pt-br/screens/dashboard/tours.ts
+++ src/configs/i18n/locales/pt-br/screens/dashboard/tours.ts
@@ -15,4 +15,10 @@ export const ToursTranslations = {
         "Insira abaixo os possíveis endereços de desembarque para opção na compra do passeio",
     },
   },
+  text_select_information:
+    "Selecione quais passeios terão as informações compartilhadas",
+  text_fill_information: "Preencha os dados abaixo para criar um novo passeio:",
+  title_already_exclude: "Você deseja excluir este passeio?",
+  text_already_exclude:
+    "Ao continuar, todos os dados dos passeios selecionados serão excluídos, e eles não poderão mais acessar até que sejam recadastrados.",
 };
