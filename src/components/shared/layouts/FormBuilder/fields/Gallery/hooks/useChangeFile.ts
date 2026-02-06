66c4f0894f9a7bf07a42e08484c4faa4cb7cee8a
igfernandes <fernandes7.1@outlook.com>
2026-02-06 07:11:26 -0300

    chore: problem with file

--- src/components/shared/layouts/FormBuilder/fields/Gallery/hooks/useChangeFile.ts
+++ src/components/shared/layouts/FormBuilder/fields/Gallery/hooks/useChangeFile.ts
@@ -1,12 +1,7 @@
 import { ChangeEvent, useState } from "react";
 import { GalleryFileShape } from "../type";
-import { useI18n } from "@contexts/I18n";
-
-const MAX_FILE_SIZE_MB = 5; // limite por arquivo
-const MAX_TOTAL_SIZE_MB = 20; // limite total
 
 export function useChangeFile() {
-  const { t } = useI18n();
   const [files, setFiles] = useState<Array<GalleryFileShape>>([]);
 
   const handleDelete = (fileIndex: number) => {
@@ -23,40 +18,7 @@ export function useChangeFile() {
     const changeFiles = e.currentTarget.files;
     if (!changeFiles) return;
 
-    const maxFileSize = MAX_FILE_SIZE_MB * 1024 * 1024;
-    const maxTotalSize = MAX_TOTAL_SIZE_MB * 1024 * 1024;
-
-    const currentTotalSize = files.reduce(
-      (acc, file) => acc + file.ref.size,
-      0
-    );
-
     const newFiles = Array.from(changeFiles);
-
-    for (const file of newFiles) {
-      if (file.size > maxFileSize) {
-        alert(
-          t("Validations.file_size_exceeded", {
-            name: file.name,
-            size: MAX_FILE_SIZE_MB,
-          })
-        );
-        return;
-      }
-    }
-
-    const newFilesTotalSize = newFiles.reduce(
-      (acc, file) => acc + file.size,
-      0
-    );
-    if (currentTotalSize + newFilesTotalSize > maxTotalSize) {
-      alert(
-        t("Validations.file_total_size_exceeded", {
-          size: MAX_TOTAL_SIZE_MB,
-        })
-      );
-      return;
-    }
 
     const galleryFiles = newFiles.map((file) => ({
       name: file.name,
